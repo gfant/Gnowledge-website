c730a74784713dd203022715c90d82c7b51fc1c1
iam-agf <agf0710@gmail.com>
2024-06-04 01:21:50 -0600

    Block for showing answers of a question

---
+++ src/components/AnswerShowcase.tsx
@@ -0,0 +1,50 @@
+import { Col, Divider, FlexboxGrid, Grid, Heading, Row } from 'rsuite';
+import FlexboxGridItem from 'rsuite/esm/FlexboxGrid/FlexboxGridItem';
+import { Answer} from '../pieces/Realm.types';
+import 'rsuite/Divider/styles/index.css';
+import 'rsuite/Heading/styles/index.css';
+import { Link } from 'react-router-dom';
+import { useContext, useEffect, useState } from 'react';
+import ProviderContext from '../context/ProviderContext';
+import AccountContext from '../context/AccountContext';
+import { parseJSONResponse } from '../pieces/supportFuns';
+
+const AnswerShowcase = ({ a }: { a: Answer }) => {
+
+    const author = a.author.substring(0, 6) + "..." + a.author.substring(a.author.length - 6, a.author.length)
+    return (
+        <>
+            <Divider />
+            <Grid fluid>
+                <Row>
+                    <Col>
+                        <FlexboxGrid style={{ alignItems: "center" }}>
+                            <FlexboxGridItem colspan={2} style={{ border: "3px solid black", padding: "10px" }}>
+                                <Heading level={3}>
+                                    {a.score}
+                                </Heading>
+                            </FlexboxGridItem>
+                            <FlexboxGridItem style={{ marginLeft: "20px", alignItems: "center" }}>
+                                <Heading level={4}>
+                                    {a.response}
+                                </Heading>
+                            </FlexboxGridItem>
+                            
+                        </FlexboxGrid>
+                    </Col>
+                    <Col></Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col xs={6} xsPull={18}>
+                        <a href={"member/" + a.author}>
+                            {author}
+                        </a>, {a.createdOn}
+                    </Col>
+                </Row>
+            </Grid>
+        </>
+    );
+};
+
+export default AnswerShowcase;
