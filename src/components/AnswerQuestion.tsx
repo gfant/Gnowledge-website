fd488d4fcec10d25cd6bd6717b90d6020ba8743d
iam-agf <agf0710@gmail.com>
2024-06-04 01:56:22 -0600

    Minor fixes

--- src/components/AnswerQuestion.tsx
+++ src/components/AnswerQuestion.tsx
@@ -1,23 +1,20 @@
-import { Button, Col, FlexboxGrid, Input } from 'rsuite';
+import { Button, FlexboxGrid, Input } from 'rsuite';
 
 import 'rsuite/Divider/styles/index.css';
 import 'rsuite/Heading/styles/index.css';
 import FlexboxGridItem from 'rsuite/esm/FlexboxGrid/FlexboxGridItem';
 import { useContext, useState } from 'react';
-import ProviderContext from '../context/ProviderContext';
 import AccountContext from '../context/AccountContext';
 import { AdenaService } from '../services/adena/adena';
 import { EMessageType } from '../services/adena/adena.types';
 import config from '../config';
 
 const AnswerQuestion = ({ qId }: { qId: string }) => {
-    const { provider } = useContext(ProviderContext);
     const { address } = useContext(AccountContext);
     const [qAnswer, setQAnswer] = useState("")
-
+    
     const SendAnswer = async () => {
         if (address && qAnswer.length !== 0) {
-            console.log({qId, qAnswer})
             let response = await AdenaService.sendTransaction(
                 [
                     {
@@ -43,10 +40,10 @@ const AnswerQuestion = ({ qId }: { qId: string }) => {
     }
 
     return <>
-        <FlexboxGrid style={{ alignItems: "center", justifyContent: "center" }}>
+        <FlexboxGrid style={{ alignItems: "center", justifyContent: "center", marginTop:"50px"}}>
             <FlexboxGridItem >
                 <FlexboxGrid style={{ flexDirection: "column" }}>
-                    <Input as="textarea" cols={80} rows={12} placeholder="Answer the question" value={qAnswer} onChange={value => setQAnswer(value)}/>
+                    <Input as="textarea" cols={80} rows={12} placeholder="Answer the question" value={qAnswer} onChange={value => setQAnswer(value)} />
                     <Button onClick={SendAnswer}>Answer Question</Button>
                 </FlexboxGrid>
             </FlexboxGridItem>
--- src/pieces/supportFuns.tsx
+++ src/pieces/supportFuns.tsx
@@ -11,13 +11,11 @@ export const parseResponse = (response: string): string => {
 };
 
 export const parseJSONResponse = (response: string): string => {
-    console.log({response, 1:1 })
     var content = response.replace(/\\n/g, '').replace(/\\t/g, '').replace(/\\"/g, '"');
     let left = 0
     let right = content.length
     while (0 < right && content[right] != "}") right -= 1
     while (left < content.length && content[left] != "{") left += 1
     content = content.substring(left, right+1);
-    console.log('final',{content})
     return content
 }
