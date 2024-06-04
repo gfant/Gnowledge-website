d7741c5dcbfd4c792aba75991034f392f865109b
iam-agf <agf0710@gmail.com>
2024-06-04 00:47:40 -0600

    AnswerBox for Question view

---
+++ src/components/AnswerQuestion.tsx
@@ -0,0 +1,57 @@
+import { Button, Col, FlexboxGrid, Input } from 'rsuite';
+
+import 'rsuite/Divider/styles/index.css';
+import 'rsuite/Heading/styles/index.css';
+import FlexboxGridItem from 'rsuite/esm/FlexboxGrid/FlexboxGridItem';
+import { useContext, useState } from 'react';
+import ProviderContext from '../context/ProviderContext';
+import AccountContext from '../context/AccountContext';
+import { AdenaService } from '../services/adena/adena';
+import { EMessageType } from '../services/adena/adena.types';
+import config from '../config';
+
+const AnswerQuestion = ({ qId }: { qId: string }) => {
+    const { provider } = useContext(ProviderContext);
+    const { address } = useContext(AccountContext);
+    const [qAnswer, setQAnswer] = useState("")
+
+    const SendAnswer = async () => {
+        if (address && qAnswer.length !== 0) {
+            console.log({qId, qAnswer})
+            let response = await AdenaService.sendTransaction(
+                [
+                    {
+                        type: EMessageType.MSG_CALL,
+                        value: {
+                            caller: address,
+                            send: '',
+                            pkg_path: config.REALM_PATH,
+                            func: 'AnswerQuestion',
+                            args: [
+                                qAnswer,
+                                qId,
+                            ]
+                        }
+                    }
+                ],
+                2000000
+            )
+            if (response !== null) {
+                console.log("Answer sent!")
+            }
+        }
+    }
+
+    return <>
+        <FlexboxGrid style={{ alignItems: "center", justifyContent: "center" }}>
+            <FlexboxGridItem >
+                <FlexboxGrid style={{ flexDirection: "column" }}>
+                    <Input as="textarea" cols={80} rows={12} placeholder="Answer the question" value={qAnswer} onChange={value => setQAnswer(value)}/>
+                    <Button onClick={SendAnswer}>Answer Question</Button>
+                </FlexboxGrid>
+            </FlexboxGridItem>
+        </FlexboxGrid>
+    </>
+};
+
+export default AnswerQuestion;
