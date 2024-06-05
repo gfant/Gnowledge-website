13497e0e59b16920d3d634fca1f700e6d9114828
iam-agf <agf0710@gmail.com>
2024-06-05 01:00:54 -0600

    Home View update

--- src/pages/pageHome.tsx
+++ src/pages/pageHome.tsx
@@ -1,31 +1,9 @@
-import { Button, Col, Container, FlexboxGrid, Heading, HeadingGroup, Row } from 'rsuite';
-
-import 'rsuite/FlexboxGrid/styles/index.css';
-import 'rsuite/Heading/styles/index.css';
-import 'rsuite/HeadingGroup/styles/index.css';
 import Questions from '../components/Questions';
-import Connect from '../services/adena/connectButton';
-import { Link } from 'react-router-dom';
-import { GoToMakeQuestion } from '../components/GoToMakeQuestion';
+import DefaultView from '../components/MainView';
 
 const PageHome = () => {
     return (
-        <div>
-            <FlexboxGrid>
-                <FlexboxGrid.Item colspan={6}>
-                    <GoToMakeQuestion/>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={12}>
-                    <Container>
-                        <Link to="/">
-                            <Heading level={1}>Gnowledge</Heading>
-                        </Link>
-                        <Questions />
-                    </Container>
-                </FlexboxGrid.Item>
-                <FlexboxGrid.Item colspan={6}><Connect /></FlexboxGrid.Item>
-            </FlexboxGrid>
-        </div>
+        <DefaultView component={Questions}/>
     );
 };
 
